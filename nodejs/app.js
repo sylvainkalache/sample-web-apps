af0ab1dd754342b94ae347e664f2d925799590ed
Sylvain Kalache <sylvain@kalache.fr>
2023-08-03 18:06:22 -0400

    Adding Node.js app

---
+++ nodejs/app.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const port = process.env.PORT || 8080
+
+const requestHandler = (request, response) => {
+    response.end(`Hello world!\nNode.js Version: ${process.version}\n`)
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+})
